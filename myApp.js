038c730033d06e5c8b997bbb21dee8c0c9aae080
esepgit <150302936+esepgit@users.noreply.github.com>
2024-01-25 10:53:43 -0300

    route /name

--- myApp.js
+++ myApp.js
@@ -2,6 +2,7 @@ let express = require('express');
 let app = express();
 require('dotenv').config();
 
+//middleware
 app.use(function(req, res, next) {
   const method = req.method;
   const path = req.path;
@@ -11,14 +12,17 @@ app.use(function(req, res, next) {
   next();
 });
 
+//ruta del css
 app.use('/public', express.static(__dirname + '/public'));
 
 console.log("Hello World");
 
+//servidor de archivos html
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+//res json en base a variable de entorno
 app.get('/json', function(req, res) {
   const note = process.env.MESSAGE_STYLE == "uppercase" ?
     {message: "HELLO JSON"} :
@@ -27,6 +31,7 @@ app.get('/json', function(req, res) {
   res.json(note);
 });
 
+//manejadores encadenados
 app.get('/now', function(req, res, next) {
   req.time = new Date().toString();
   next();
@@ -34,10 +39,22 @@ app.get('/now', function(req, res, next) {
   res.json({time: req.time})
 });
 
+//leer parametros de cliente metodo 1
 app.get('/:word/echo', function(req, res) {
   const word = req.params.word;
   res.json({echo: word});
 });
+
+//leer parametros de cliente metodo 2
+app.route('/name')
+  .get(function(req, res) {
+    const firstName = req.query.first;
+    const lastName = req.query.last;
+
+    res.json({ name: `${firstName} ${lastName}` });
+  })
+
+
 
 
 
