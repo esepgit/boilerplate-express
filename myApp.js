f886c24a4708cf5a0b787a1d8472cc3b8301ba3a
esepgit <150302936+esepgit@users.noreply.github.com>
2024-01-25 00:08:15 -0300

    /json updated

--- myApp.js
+++ myApp.js
@@ -1,6 +1,9 @@
 let express = require('express');
 let app = express();
 require('dotenv').config();
+const note = {
+  message: "Hello json",
+};
 
 app.use('/public', express.static(__dirname + '/public'));
 
@@ -10,11 +13,11 @@ app.get('/', function(req, res) {
 });
 
 app.get('/json', function(req, res) {
-  if (process.env.MESSAGE_STYLE === 'uppercase') {
-    res.json({"message": "HELLO JSON"});
-  } else {
-    res.json({"message": "Hello json"});
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    note.message = note.message.toUpperCase();
   }
+
+  rs.json(note);
 });
 
 
