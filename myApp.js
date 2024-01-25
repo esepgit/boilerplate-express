d7b7664d10640c8af68834a0d2a128ef35bc5f37
esepgit <150302936+esepgit@users.noreply.github.com>
2024-01-24 23:07:59 -0300

    /json

--- myApp.js
+++ myApp.js
@@ -6,7 +6,11 @@ app.use('/public', express.static(__dirname + '/public'));
 console.log("Hello World");
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
-})
+});
+
+app.get('/json', function(req, res) {
+  res.json({"message": "Hello json"});
+});
 
 
 
