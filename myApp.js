76c7e577e999a452e5bbc8e759ddce866c6916d0
esepgit <150302936+esepgit@users.noreply.github.com>
2024-01-25 10:07:24 -0300

    middleware

--- myApp.js
+++ myApp.js
@@ -2,52 +2,34 @@ let express = require('express');
 let app = express();
 require('dotenv').config();
 
+app.use(function(req, res, next) {
+  const method = req.method;
+  const path = req.path;
+  const ip = req.ip;
+
+  console.log(`${method} ${path} - ${ip}`);
+  next();
+});
+
 app.use('/public', express.static(__dirname + '/public'));
 
 console.log("Hello World");
+
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 app.get('/json', function(req, res) {
-  if (process.env.MESSAGE_STYLE == "uppercase") {
-    res.json({"message": "HELLO JSON"});
-  } 
+  const note = process.env.MESSAGE_STYLE == "uppercase" ?
+    {"message": "HELLO JSON"} :
+    {"message": "Hello json"}
 
-  res.json({"message": "Hello json"});
-});
-
-
+  res.json(note);
+})
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
